d3e1e167635b7613df2a57dcac41683877222b06
Manav Sanghvi <mannyjainlakes@gmail.com>
2024-10-24 20:43:57 -0400

    initial commit

---
+++ src/example.py
@@ -0,0 +1,8 @@
+class Example:
+
+    def __init__(self, example_parameter):
+        self.__example_parameter = example_parameter
+
+    def example(self):
+        print(self.__example_parameter)
+
