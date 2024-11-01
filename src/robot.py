f78fa029c7caae3a02ec8484b66952bfc7ca0be8
heymicah <micahtam70@gmail.com>
2024-10-31 21:36:45 -0400

    created template for async function calling

--- src/robot.py
+++ src/robot.py
@@ -1,13 +1,40 @@
 
 from example import Example
+import asyncio
+from datetime import datetime
+
+async def test_script1():
+    print("1 started")
+    await asyncio.sleep(1)
+    print("1 finished")
+
+async def test_script2():
+    print("2 started")
+    await asyncio.sleep(2)
+    print("2 finished")
+
+async def test_script3():
+    print("3 started")
+    await asyncio.sleep(3)
+    print("3 finished")
 
 # Gonna be in some sort of loop because we are constantly running
-def main():
+async def main():
     while True:
-        ex = Example('Test')
 
-        # Might have to run scripts async
-        ex.example()
+        async with asyncio.TaskGroup() as tg:
+            task1 = tg.create_task(
+                test_script1()
+            )
+            task2 = tg.create_task(
+                test_script2()
+            )
+            task3 = tg.create_task(
+                test_script3()
+            )
+            print(datetime.now())
+
+        print(datetime.now())
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
