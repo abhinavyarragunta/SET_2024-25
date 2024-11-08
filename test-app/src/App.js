08a420d88aa3252ed1efd0c42beaf4a7e8564f76
Manav Sanghvi <mannyjainlakes@gmail.com>
2024-11-07 19:14:00 -0500

    Cleaned upp app.js

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -4,10 +4,9 @@ import './App.css';
 
 function App() {
   const [ipAddress, setIpAddress] = useState('');
-  const flaskServerUrl = "http://127.0.0.1:8000";  // Your Flask server's IP and port
+  const flaskServerUrl = "http://127.0.0.1:8000";
 
   useEffect(() => {
-    // Fetch the IP address from the Flask backend
     fetch(`${flaskServerUrl}/get-ip`)
       .then(response => response.json())
       .then(data => setIpAddress(data.ip))
@@ -17,15 +16,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className="App-link">
-          {ipAddress ? `Server IP: ${ipAddress}` : "Loading..."}
-        </p>
-
-        {/* Display the video feed from Flask */}
         <div className="video-feed">
           <h2>Live Video Feed</h2>
           <img
