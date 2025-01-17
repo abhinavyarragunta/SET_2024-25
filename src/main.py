3dee4b7bc423ef0413abe62a8befa2ea2594dbeb
Abhinav Yarragunta <148981364+abhinavyarragunta@users.noreply.github.com>
2025-01-16 21:41:03 -0500

    Resize resolution of frame

--- src/main.py
+++ src/main.py
@@ -9,6 +9,10 @@ def main():
     # Configure logging
     logging.basicConfig(level=logging.INFO)
 
+    import torch
+    print("CUDA Available:", torch.cuda.is_available())
+    print("Device Name:", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "No GPU detected")
+
     # Path to the YOLO pose estimation model
     model_path = '../yolo11x-pose (1).pt'
 
@@ -20,6 +24,9 @@ def main():
     if not video_capture.isOpened():
         logging.error("Error: Could not open webcam.")
         return
+
+    video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
     logging.info("Fall Detection System is running. Press 'q' to quit.")
 
@@ -35,6 +42,7 @@ def main():
 
         # Display the processed frame in a window
         cv2.imshow('Fall Detection System', processed_frame)
+        #cv2.resizeWindow('Fall Detection System', 1080, 1920)
 
         # Exit the loop if 'q' is pressed
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -44,6 +52,7 @@ def main():
     # Release the video capture object and close all OpenCV windows
     video_capture.release()
     cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     main()
